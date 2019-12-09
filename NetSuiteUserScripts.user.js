7562882dbd50801ed440217a0192b41ac6e14046
nschumer-pcc <58708864+nschumer-pcc@users.noreply.github.com>
2019-12-09 16:42:51 -0500

    Update NetSuiteUserScripts.user.js

--- NetSuiteUserScripts.user.js
+++ NetSuiteUserScripts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name PCC Netsuite UserScript
-// @version 0.1
+// @version 1.0
 // @description Float item sublist header on scroll
 // @updateURL    https://github.com/nschumer-pcc/Netsuite-User-Scripts/blob/master/NetSuiteUserScripts.user.js
 // @downloadURL  https://github.com/nschumer-pcc/Netsuite-User-Scripts/blob/master/NetSuiteUserScripts.user.js
