50d7030e12807316b29c177b0282f01877d40b14
nschumer-pcc <58708864+nschumer-pcc@users.noreply.github.com>
2019-12-09 16:22:17 -0500

    Add files via upload

---
+++ NetSuiteUserScripts.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name PCC Netsuite UserScript
+// @version 0.1
+// @description Float item sublist header on scroll
+// @match https://*.netsuite.com/*
+// grant none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+(function($, undefined) {
+    $(document).ready(checkItemTable);
+
+    function checkItemTable() {
+        if ($('.uir-machine-table-container').outerHeight() > 100) {
+            const windowHeight = $(window).height();
+            $('.uir-machine-table-container')
+                .filter((index, elem) => $(elem).height() > windowHeight)
+                .css('height', '70vh')
+                .on('scroll', (event) => {
+                    const headerElem = $(event.target).find('.uir-machine-headerrow');
+                    headerElem.css('transform', `translate(0, ${event.target.scrollTop}px)`);
+                })
+        } else {
+            setTimeout(checkItemTable, 50);
+        }
+    }
+})(window.jQuery.noConflict(true));
+
+
